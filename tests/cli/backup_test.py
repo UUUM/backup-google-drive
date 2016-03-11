00b9c5515aba7d83e42a32b004c551d28cad8ca2
Masato Bito <masato@bz2.jp>
2016-03-11 18:36:58 +0900

    add backup_test.py

--- tests/cli/backup_test.py
+++ tests/cli/backup_test.py
@@ -1,17 +1,11 @@
-import unittest
-
 import gdbackup.cli.backup
 from gdbackup.cli.backup import Backup
 from gdbackup.config import Config
 
 
-class BackupTest(unittest.TestCase):
+class TestBackupTest:
     def test_init(self):
         backup = Backup()
         assert isinstance(backup.backup, gdbackup.google.backup.Backup)
         assert '_print' == backup.backup.callback.__name__
         assert isinstance(backup.config, Config)
-
-
-if __name__ == '__main__':
-    unittest.main()
