f471f5959c7ac6b10a7513ea933247583539889e
Masato Bito <masato@bz2.jp>
2016-03-11 18:29:24 +0900

    add config test

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,19 +1,14 @@
 import os
-import unittest
 
-import gdbackup.config
+import gdbackup
 from gdbackup.config import Config
 
 gdbackup.config.CONFIG_FILE = os.path.join(os.path.dirname(__file__), 'config.yml')
 
 
-class ConfigTest(unittest.TestCase):
+class TestConfig:
     def test_init(self):
         config = Config()
         assert 'sync' in config
         assert 'source_ids' in config['sync']
         assert 'destination_id' in config['sync']
-
-
-if __name__ == '__main__':
-    unittest.main()
