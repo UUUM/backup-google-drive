7cf9b23d89ae715836505c859ef69d2defd7650b
Masato Bito <masato@bz2.jp>
2016-03-14 14:52:43 +0900

    just simply sync

--- gdbackup/google/backup.py
+++ gdbackup/google/backup.py
@@ -1,6 +1,5 @@
 from dateutil import parser
 import os
-import time
 
 from gdbackup.google.drive import Drive, Resource
 from gdbackup.google.finished_folders import FinishedFolders
@@ -9,79 +8,62 @@ FinishedFolders.db_file_name = 'backup.db'
 
 
 class Backup:
-    def __init__(self):
+    finished_folders = None
+
+    def __init__(self, src, dest, sync_id=None):
         self.callback = print_none
         self.drive = Drive()
 
-    def create_date_folder(self, res):
-        folder_name = time.strftime("%Y-%m-%d")
-        return res.create_folder(folder_name, unique=True)
+        self.src = self._init_resource(src)
+        self.dest = self._init_resource(dest)
 
-    def find_prev_folder(self, dest_folder, cur_folder):
-        cur_created_time = parser.parse(cur_folder.createdTime)
-        for file in dest_folder.list(order_by='createdTime desc'):
-            if not file.is_folder():
-                continue
+        if sync_id:
+            self.finished_folders = FinishedFolders(self.dest.id)
+            self.finished_folders.load()
 
-            if cur_folder.id == file.id:
-                continue
-
-            if cur_created_time <= parser.parse(file.createdTime):
-                continue
-
-            return file
-
-        return None
-
-    def merge(self, src, dest):
-        src = self._init_resource(src)
-        dest = self._init_resource(dest)
-
-        sync = Sync(self, src, dest)
-        sync.callback = self.callback
-        sync.merge()
-
-    def sync(self, src, dest, comp=None):
-        src = self._init_resource(src)
-        dest = self._init_resource(dest)
-        comp = self._init_resource(comp)
-
-        sync = Sync(self, src, dest, comp)
-        sync.callback = self.callback
-        sync.sync()
+    def sync(self):
+        self._sync(self.src, self.dest, '')
+        return self
 
     def _init_resource(self, res):
-        if isinstance(res, Resource) or res is None:
-            return res
+        if isinstance(res, Resource):
+            return self.drive.open(res.id)
         elif isinstance(res, str):
             return self.drive.open(res)
         else:
             raise ValueError('Value must be Resource object or resource id')
 
+    def _sync(self, src_res, dest_res, folder_name):
+        folder_name = os.path.join(folder_name, dest_res.name)
 
-class Sync:
-    finished_folders = None
+        for src_item in src_res.list():
+            self._sync_item(src_item, dest_res, folder_name)
 
-    def __init__(self, drive, src, dest, comp=None):
-        self.drive = drive
-        self.callback = print_none
+    def _sync_item(self, src_item, dest_res, folder_name):
+        if src_item.is_folder():
+            self._sync_folder(src_item, dest_res, folder_name)
+        elif src_item.is_app():
+            pass
+        else:
+            self._sync_file(src_item, dest_res, folder_name)
 
-        self.src = src
-        self.dest = dest
-        self.comp = comp
+    def _sync_folder(self, src_item, dest_res, folder_name):
+        if self.finished_folders is not None and src_item.id in self.finished_folders:
+            self.callback(src_item, folder_name, state='skip')
+            return
 
-    def merge(self):
-        self._sync_file = self._callback_merge_file
-        self._sync(self.src, self.dest, self.comp, '')
+        dest_folder = dest_res.find_folder(src_item.name)
+        if not dest_folder:
+            dest_folder = dest_res.create_folder(src_item.name)
 
-    def sync(self):
-        self.finished_folders = FinishedFolders(self.dest.id)
-        self.finished_folders.load()
+        self.callback(src_item, folder_name, state='folder')
+        self._sync(src_item, dest_folder, folder_name)
 
-        self._sync_file = self._callback_sync_file
-        self._sync(self.src, self.dest, self.comp, '')
+        if self.finished_folders is not None:
+            self.finished_folders.add(src_item.id)
+            self.finished_folders.save()
 
-    def _callback_merge_file(self, src_item, dest_res, comp_res, folder_name):
+    def _sync_file(self, src_item, dest_res, folder_name):
         dest_file = dest_res.find(src_item.name, mime_type=src_item.mimeType)
         if not dest_file:
             self.callback(src_item, folder_name, state='new')
@@ -92,61 +74,6 @@ class Sync:
             src_item.copy_to(dest_res)
         else:
             self.callback(src_item, folder_name, state='skip')
-
-    def _callback_sync_file(self, src_item, dest_res, comp_res, folder_name):
-        if dest_res.find(src_item.name, mime_type=src_item.mimeType):
-            return
-
-        if comp_res:
-            comp_file = comp_res.find(src_item.name, mime_type=src_item.mimeType)
-        else:
-            comp_file = None
-
-        if comp_file:
-            if parser.parse(comp_file.createdTime) < parser.parse(src_item.modifiedTime):
-                self.callback(src_item, folder_name, state='update')
-                src_item.copy_to(dest_res)
-            else:
-                self.callback(src_item, folder_name, state='add')
-                comp_file.add_to(dest_res)
-        else:
-            self.callback(src_item, folder_name, state='update')
-            src_item.copy_to(dest_res)
-
-    def _sync(self, src_res, dest_res, comp_res, folder_name):
-        folder_name = os.path.join(folder_name, dest_res.name)
-
-        for src_item in src_res.list():
-            self._sync_item(src_item, dest_res, comp_res, folder_name)
-
-    def _sync_item(self, src_item, dest_res, comp_res, folder_name):
-        if src_item.is_folder():
-            self._sync_folder(src_item, dest_res, comp_res, folder_name)
-        elif src_item.is_app():
-            pass
-        else:
-            self._sync_file(src_item, dest_res, comp_res, folder_name)
-
-    def _sync_folder(self, src_item, dest_res, comp_res, folder_name):
-        if self.finished_folders is not None and src_item.id in self.finished_folders:
-            self.callback(src_item, folder_name, state='skip')
-            return
-
-        dest_folder = dest_res.find_folder(src_item.name)
-        if not dest_folder:
-            dest_folder = dest_res.create_folder(src_item.name)
-
-        if comp_res:
-            comp_folder = comp_res.find_folder(src_item.name)
-        else:
-            comp_folder = None
-
-        self.callback(src_item, folder_name, state='folder')
-        self._sync(src_item, dest_folder, comp_folder, folder_name)
-
-        if self.finished_folders is not None:
-            self.finished_folders.add(src_item.id)
-            self.finished_folders.save()
 
 
 def print_none(src_item, folder_name, state=''):
--- tests/google/backup_test.py
+++ tests/google/backup_test.py
@@ -6,121 +6,68 @@ import time
 
 import pytest
 
-from gdbackup.google.backup import Backup, Sync
+from gdbackup.google.backup import Backup
 from gdbackup.google.drive import Drive, Resource
+from gdbackup.google.finished_folders import FinishedFolders
 
 
-@pytest.mark.usefixtures('test_folder', 'dest_folder')
+@pytest.mark.usefixtures('test_folder', 'src_folder', 'src_file', 'delete_db_file')
 class TestBackup:
     def test_init(self):
-        backup = Backup()
+        backup = Backup('xxxxxx', 'yyyyyy')
         assert hasattr(backup.callback, '__call__')
         assert isinstance(backup.drive, Drive)
+        assert isinstance(backup.src, Resource)
+        assert isinstance(backup.dest, Resource)
+        assert backup.finished_folders is None
 
-    def test_create_date_folder(self, dest_folder):
-        backup = Backup()
-        res = backup.drive.open(dest_folder.id)
+        backup = Backup('xxxxxx', 'yyyyyy', 'sync_id')
+        assert isinstance(backup.finished_folders, FinishedFolders)
 
-        folder1 = backup.create_date_folder(res)
-        assert isinstance(folder1, Resource)
-        assert time.strftime("%Y-%m-%d") == folder1.name
+    def test_sync(self, test_folder, src_folder, src_file):
+        drive = Drive()
+        test_folder = drive.open(test_folder.id)
+        src_folder = drive.open(src_folder.id)
 
-        folder2 = backup.create_date_folder(res)
-        assert isinstance(folder1, Resource)
-        assert folder1.id == folder2.id
-
-    def test_find_prev_folder(self, test_folder):
-        backup = Backup()
-        res = test_folder.create_folder('test_prev_folder')
-
-        folders = []
-        for i in range(3):
-            folders.append(res.create_folder('folder%d' % i))
-            time.sleep(1)
-
-        folder = backup.find_prev_folder(res, folders[2])
-        assert folders[1].id == folder.id
-
-        folder = backup.find_prev_folder(res, folders[1])
-        assert folders[0].id == folder.id
-
-        folder = backup.find_prev_folder(res, folders[0])
-        assert folder is None
-
-        for folder in folders:
-            folder.delete()
-        res.delete()
-
-
-@pytest.mark.usefixtures('dest_folder', 'src_folder', 'src_file', 'delete_db_file')
-class TestSync:
-    def test_merge(self, dest_folder, src_folder):
-        backup = Backup()
-        drive = backup.drive
-        src_res = drive.open(src_folder.id)
-        dest_res = drive.open(dest_folder.id)
-        dest_folder = backup.create_date_folder(dest_res)
+        sync_id = '%d' % time.time()
+        dest_folder = test_folder.create_folder(sync_id)
 
         # sync to empty folder
-        Sync(drive, src_res, drive.open(dest_folder.id)).merge()
-        compare_folder(src_res, drive.open(dest_folder.id))
-
-        # remove some files and merge
-        dest_folder = drive.open(dest_folder.id)
-        dest_folder.find('テスト', mime_type='text/plain').delete()
-        dest_folder.find('テスト', mime_type='text/csv').delete()
-        Sync(drive, src_res, drive.open(dest_folder.id)).merge()
-        compare_folder(src_res, drive.open(dest_folder.id))
-
-        # create a new file and merge
-        new_file = src_res.create('test.txt', content='test')
-        Sync(drive, src_res, drive.open(dest_folder.id)).merge()
-        compare_folder(src_res, drive.open(dest_folder.id))
-        new_file.delete()
-
-    def test_sync(self, dest_folder, src_folder):
-        backup = Backup()
-        drive = backup.drive
-        src_res = drive.open(src_folder.id)
-        dest_res = drive.open(dest_folder.id)
-        dest_folder = backup.create_date_folder(dest_res)
-
-        # sync to empty folder
-        sync = Sync(drive, src_res, drive.open(dest_folder.id))
-        sync.sync()
-        assert len(sync.finished_folders) == 1
-        assert src_res.find('フォルダ').id in sync.finished_folders
-        compare_folder(src_res, drive.open(dest_folder.id))
+        backup = Backup(src_folder, dest_folder, sync_id).sync()
+        assert len(backup.finished_folders) == 1
+        assert src_folder.find('フォルダ').id in backup.finished_folders
+        compare_folder(src_folder, dest_folder)
 
         # remove some files and sync
         dest_folder = drive.open(dest_folder.id)
         dest_folder.find('テスト', mime_type='text/plain').delete()
         dest_folder.find('テスト', mime_type='text/csv').delete()
-        sync = Sync(drive, src_res, drive.open(dest_folder.id))
-        sync.sync()
-        compare_folder(src_res, drive.open(dest_folder.id))
+        Backup(src_folder, dest_folder, sync_id).sync()
+        compare_folder(src_folder, dest_folder)
+
+        # create a new file and sync
+        new_file_name = '%s.txt' % sync_id
+        new_file = src_folder.create(new_file_name, content='test')
+        Backup(src_folder, dest_folder, sync_id).sync()
+        compare_folder(src_folder, dest_folder)
+
+        # remove a new file and sync and a new file still exists
+        new_file.delete()
+        Backup(src_folder, dest_folder, sync_id).sync()
+        dest_folder = drive.open(dest_folder.id)
+        assert isinstance(dest_folder.find(new_file_name), Resource)
 
         # skip synced folder
         dest_folder.find('フォルダ').find('テストsub').delete()
-        sync = Sync(drive, src_res, drive.open(dest_folder.id))
-        sync.sync()
-        assert len(sync.finished_folders) == 1
+        backup = Backup(src_folder, dest_folder, sync_id).sync()
+        assert len(backup.finished_folders) == 1
         assert drive.open(dest_folder.id).find('フォルダ').find('テストsub') is None
-
-        # sync to a new folder while comparing to a previous folder
-        new_folder = dest_res.create_folder('new_folder')
-        sync = Sync(drive, src_res, new_folder, drive.open(dest_folder.id))
-        sync.sync()
-        new_folder = drive.open(new_folder.id)
-        dest_folder = drive.open(dest_folder.id)
-        file1 = dest_folder.find('テスト', mime_type='text/plain')
-        file2 = new_folder.find('テスト', mime_type='text/plain')
-        assert file1.id == file2.id
-        file = drive.open(new_folder.id).find('フォルダ').find('テストsub')
-        assert isinstance(file, Resource)
 
 
 def compare_folder(src_res, dest_res):
+    drive = Drive()
+    dest_res = drive.open(dest_res.id)
+    src_res = drive.open(src_res.id)
     for src_item in src_res.list():
         if src_item.is_folder():
             dest_folder = dest_res.find_folder(src_item.name)
