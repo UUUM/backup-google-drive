3fce0886f81fd8ee471375df7ac868a6f77b9e39
Masato Bito <masato@bz2.jp>
2016-03-22 16:41:53 +0900

    disable pandoc

--- setup.py
+++ setup.py
@@ -1,17 +1,5 @@
 from setuptools import setup
 
-try:
-    import pypandoc
-
-    def read_md(file):
-        return pypandoc.convert(file, 'rst')
-except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
-
-    def read_md(file):
-        return open(file, 'r').read()
-
-
 setup(
     name='gdsync',
     version='0.0.1',
@@ -20,7 +8,6 @@ setup(
     author_email='bito_m@uuum.jp',
     url='https://github.com/uuum/gdsync',
     license='MIT',
-    long_description=read_md('README.md'),
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
 )
