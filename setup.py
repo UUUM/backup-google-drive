5b6630fa8a225f78d8c888f3b2e652b212390a2f
Masato Bito <masato@bz2.jp>
2016-03-22 16:34:25 +0900

    add pypandoc

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
 from setuptools import setup
 
+try:
+    import pypandoc
+
+    def read_md(file):
+        return pypandoc.convert(file, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+
+    def read_md(file):
+        return open(file, 'r').read()
+
+
 setup(
+    name='gdsync',
+    version='0.0.1',
+    description='Google Drive Synchronizer',
+    author='Masato Bito',
+    author_email='bito_m@uuum.jp',
+    url='https://github.com/uuum/gdsync',
+    license='MIT',
+    long_description=read_md('README.md'),
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
 )
